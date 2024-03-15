933d0dedc9b69d91347f14c7d21e672688881792
pnitijarasrat <puriwatds@gmail.com>
2024-03-15 20:25:25 +0700

    adjust job layout

--- src/Recruit/Job.jsx
+++ src/Recruit/Job.jsx
@@ -40,30 +40,20 @@ export default function Job({
         <>
             <div className="job-card">
                 <div style={{ width: '80%' }}>
-                    <Row align="middle" gutter={16}>
-                        <Col span={12}>
-                            <h2>
-                                <span>{department}</span>
-                                <span>{" - "}</span>
-                                <span>{name}</span>
-                            </h2>
-                        </Col>
-                        <Col span={24}>
-                            <span>
-                                {period}
-                            </span>
-                        </Col>
-                    </Row>
-                    <Row gutter={16}>
-                        <Col span={12}>
-                            <p className="job-desc">{desc}</p>
-                        </Col>
-                        <Col span={12} push={14}>
-                            <a href={link} target="_blank" rel="noreferrer">
-                                <button>Apply</button>
-                            </a>
-                        </Col>
-                    </Row>
+                    <h2>
+                        <span>{department}</span>
+                        <span>{" - "}</span>
+                        <span>{name}</span>
+                    </h2>
+                    <span>
+                        Period:   {period}
+                    </span>
+                    <div>
+                        <p className="job-desc">Description: {desc}</p>
+                        <a href={link} target="_blank" rel="noreferrer">
+                            <button>Apply</button>
+                        </a>
+                    </div>
                 </div>
             </div>
         </>
