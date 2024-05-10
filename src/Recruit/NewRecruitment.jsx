1a98c340b97e41ed1015385f1d9ef99c4cc41769
pnitijarasrat <puriwatds@gmail.com>
2024-05-10 11:03:12 +0700

    bring max score back

--- src/Recruit/NewRecruitment.jsx
+++ src/Recruit/NewRecruitment.jsx
@@ -1,97 +1,98 @@
-import { Form, Input, Select } from "antd";
+import { Form, Input, Select, InputNumber } from "antd";
 import { useNavigate, Navigate } from "react-router-dom";
 import { url } from "../const/url";
 import React, { useState } from "react";
 import MessageAPI from "../Message/Message";
 import { useAuth } from "../Authenticate/AuthProvider";
 import { isAdmin } from "../function/role";
-import { departmentOptions } from '../const/department'
-import {
-    LoadingOutlined
-} from '@ant-design/icons';
+import { departmentOptions } from "../const/department";
+import { LoadingOutlined } from "@ant-design/icons";
 
 export default function NewRecruitment() {
-    const [isAdding, setIsAdding] = useState(false)
+  const [isAdding, setIsAdding] = useState(false);
 
-    const [recruitmentForm] = Form.useForm()
-    const navigate = useNavigate()
-    const user = useAuth()
+  const [recruitmentForm] = Form.useForm();
+  const navigate = useNavigate();
+  const user = useAuth();
 
-    const rule = [{ required: true, message: 'Please Enter this Field' }]
-    const { success, error, contextHolder } = MessageAPI()
+  const rule = [{ required: true, message: "Please Enter this Field" }];
+  const { success, error, contextHolder } = MessageAPI();
 
-    if (!isAdmin(user.role)) return <Navigate to={'/'} />
+  if (!isAdmin(user.role)) return <Navigate to={"/"} />;
 
-    const addNewRecruitment = async (newRecruitment) => {
-        setIsAdding(true)
-        try {
-            const res = await fetch(`${url}/job.json`, {
-                method: 'POST',
-                body: JSON.stringify(newRecruitment),
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            })
-            if (res.ok) {
-                success('Add')
-                setTimeout(() => {
-                    navigate('/recruit')
-                }, 1000)
-            }
-        } catch (e) {
-            error('Add')
-        }
+  const addNewRecruitment = async (newRecruitment) => {
+    setIsAdding(true);
+    try {
+      const res = await fetch(`${url}/job.json`, {
+        method: "POST",
+        body: JSON.stringify(newRecruitment),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (res.ok) {
+        success("Add");
+        setTimeout(() => {
+          navigate("/recruit");
+        }, 1000);
+      }
+    } catch (e) {
+      error("Add");
     }
+  };
 
-    const handleAddNewRecruitment = () => {
-        const payload = {
-            ...recruitmentForm.getFieldsValue(),
-            status: 'pending',
-            owner: {
-                id: localStorage.getItem("user"),
-                name: localStorage.getItem("fullName")
-            }
-        }
-        addNewRecruitment(payload)
-    }
+  const handleAddNewRecruitment = () => {
+    const payload = {
+      ...recruitmentForm.getFieldsValue(),
+      status: "pending",
+      owner: {
+        id: localStorage.getItem("user"),
+        name: localStorage.getItem("fullName"),
+      },
+    };
+    addNewRecruitment(payload);
+  };
 
-    return (
-        <>
-            {contextHolder}
-            <div className="page-container">
-                <h1>New Project</h1>
-                <Form
-                    form={recruitmentForm}
-                    labelCol={{ span: 6 }}
-                    onFinish={handleAddNewRecruitment}
-                >
-                    <Form.Item label="Project Name" name="name" rules={rule}>
-                        <Input />
-                    </Form.Item>
-                    <Form.Item label="Department" name="department" rules={rule}>
-                        <Select options={departmentOptions} />
-                    </Form.Item >
-                    <Form.Item label="Period" name="period" rules={rule}>
-                        <Input />
-                    </Form.Item>
-                    <Form.Item label="Summary Description" name="desc" rules={rule}>
-                        <Input />
-                    </Form.Item>
-                    <Form.Item label="Member Required" name="memberRequired" rules={rule}>
-                        <Input />
-                    </Form.Item>
-                    <Form.Item label="JD Link" name="link" rules={rule}>
-                        <Input />
-                    </Form.Item>
-                    <Form.Item label="Line Group Link" name="lineLink" rules={rule}>
-                        <Input />
-                    </Form.Item>
-                    <div className="footer">
-                        <button>{isAdding && <LoadingOutlined />} Add New Project</button>
-                        <button onClick={() => navigate(-1)}>Back</button>
-                    </div>
-                </Form>
-            </div>
-        </>
-    )
+  return (
+    <>
+      {contextHolder}
+      <div className="page-container">
+        <h1>New Project</h1>
+        <Form
+          form={recruitmentForm}
+          labelCol={{ span: 6 }}
+          onFinish={handleAddNewRecruitment}
+        >
+          <Form.Item label="Project Name" name="name" rules={rule}>
+            <Input />
+          </Form.Item>
+          <Form.Item label="Department" name="department" rules={rule}>
+            <Select options={departmentOptions} />
+          </Form.Item>
+          <Form.Item label="Period" name="period" rules={rule}>
+            <Input />
+          </Form.Item>
+          <Form.Item label="Summary Description" name="desc" rules={rule}>
+            <Input />
+          </Form.Item>
+          <Form.Item label="Max Score" name="score" rules={rule}>
+            <InputNumber style={{ width: "100%" }} controls={false} />
+          </Form.Item>
+          <Form.Item label="Member Required" name="memberRequired" rules={rule}>
+            <Input />
+          </Form.Item>
+          <Form.Item label="JD Link" name="link" rules={rule}>
+            <Input />
+          </Form.Item>
+          <Form.Item label="Line Group Link" name="lineLink" rules={rule}>
+            <Input />
+          </Form.Item>
+          <div className="footer">
+            <button>{isAdding && <LoadingOutlined />} Add New Project</button>
+            <button onClick={() => navigate(-1)}>Back</button>
+          </div>
+        </Form>
+      </div>
+    </>
+  );
 }
